1f7f14590f72c16c8ae6dd780e5793e6af07cbb5
tenant <337447247@qq.com>
2016-09-05 13:20:21 +0800

    daguizx

--- web_news/spiders/tianya.py
+++ web_news/spiders/tianya.py
@@ -17,12 +17,21 @@ class TianyaSpider(SpiderForum):
     start_urls = (
         'http://bbs.tianya.cn/m/block.jsp',
     )
+
     def parse(self, response):
         sub_node = response.xpath('//a[re:test(@href, "list")]/@href').extract()
+        base_url = 'http://bbs.tianya.cn/'
+        for i in xrange(len(sub_node)):
+            if not sub_node[i].startswith('http'):
+                sub_node[i] = base_url+sub_node[i]
+
         return [Request(url=i, callback=self._parse_each_node) for i in sub_node]
 
     def parse_each_node(self, response):
-        self.logger.info(response.url)
+        # self.logger.info(response.url)
+        base_url = 'http://bbs.tianya.cn/'
+        posts = [Request(url=base_url+i) for i in response.xpath('//ul[@class="post-list"]/li/a[re:test(@href, "post")]/@href').extract()]
+        return posts
 
     def parse_each_item(self, response):
         pass
