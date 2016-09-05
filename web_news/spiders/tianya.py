f9c9a60be79dfd7ed38bc646caf923ee4d45bff4
tenant <337447247@qq.com>
2016-09-05 13:36:12 +0800

    tianya

--- web_news/spiders/tianya.py
+++ web_news/spiders/tianya.py
@@ -20,21 +20,21 @@ class TianyaSpider(SpiderForum):
 
     def parse(self, response):
         sub_node = response.xpath('//a[re:test(@href, "list")]/@href').extract()
-        base_url = 'http://bbs.tianya.cn/'
+        base_url = 'http://bbs.tianya.cn'
         for i in xrange(len(sub_node)):
             if not sub_node[i].startswith('http'):
                 sub_node[i] = base_url+sub_node[i]
 
-        return [Request(url=i, callback=self._parse_each_node) for i in sub_node]
+        return [Request(url=i, callback=self._parse_each_node) for i in sub_node][:10]
 
     def parse_each_node(self, response):
         # self.logger.info(response.url)
-        base_url = 'http://bbs.tianya.cn/'
-        posts = [Request(url=base_url+i) for i in response.xpath('//ul[@class="post-list"]/li/a[re:test(@href, "post")]/@href').extract()]
+        base_url = 'http://bbs.tianya.cn'
+        posts = [Request(url=base_url+i) for i in response.xpath('//ul[@class="post-list"]/li/a[re:test(@href, "post")]/@href').extract()][:1]
         return posts
 
     def parse_each_item(self, response):
-        pass
+        self.logger.info(response.url)
 
     def next_page(self, response):
         pass
